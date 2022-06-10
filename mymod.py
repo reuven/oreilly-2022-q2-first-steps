213a326c2305b698286c099cd21007109c1ad34b
Reuven Lerner <reuven@lerner.co.il>
2022-06-10 06:26:56 +0300

    gitautosync update 42

--- mymod.py
+++ mymod.py
@@ -2,5 +2,7 @@ x = 100
 
 y = [10, 20, 30]
 
+months = {'Jan':1, 'Feb':2, 'Mar':3, 'Apr':4, 'May':5, 'Jun':6}  # ignore the latter half of the year!
+
 def hello(name):
     return f'Hello, {name}!'
